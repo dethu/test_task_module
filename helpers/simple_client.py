4ff41d5eda5dbebdd9a49b53d67fd4758f409718
zolotarevr <zolotarevr@skbkontur.ru>
2019-06-11 19:33:53 +0700

    added some Exceptions, error schemas

--- helpers/simple_client.py
+++ helpers/simple_client.py
@@ -1,13 +1,17 @@
 import requests
 
 
-# todo: Вынести генерацию данных в отдельную функцию
-
 class SimpleCalculateClient(object):
     _url = ""
 
     def __init__(self, url):
         self._url = url
 
-    def calculate(self, expression):
-        return requests.post(self._url, json=expression)
+    def calculate(self, expression, timeout=None):
+        try:
+            response = requests.post(self._url, json=expression, timeout=timeout)
+        except requests.ConnectionError as connection_error:
+            print("Error Connecting:", connection_error)
+        except requests.exceptions.Timeout as timeout:
+            print("Timeout Error:", timeout)
+        return response
--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -19,22 +19,23 @@ class TestCalculate(object):
         ]
     )
     def test_should_calculate_correctly_and_return_200_while_expression_is_valid(self, left_operand, right_operand, operation, result):
-        """
-        """
         response = self._client.calculate(generate_expression(left_operand, right_operand, operation))
         assert (response.status_code == 200)
         assert_valid_schema(response.json(), get_path_to_schema_file(__file__, 'calculate_correct_200_response'))
         assert (response.json()["result"] == result)
 
     def test_should_return_400_for_expression_with_unsupported_operation(self):
-        """
-        """
         response = self._client.calculate(generate_expression(2, 2, "%"))
         assert (response.status_code == 400)
+        assert_valid_schema(response.json(), get_path_to_schema_file(__file__, 'calculate_error_400_response'))
+        # Я не уверен, что человеко-читаемые ошибки - это часть контракта.
+        assert (response.json()["error"] == "\"operation\" value must be one of \"['-', '+', '/', '*']\"")
 
     def test_should_return_400_for_invalid_expression(self):
-        """
-        """
         incorrect_expression = {"left_operand": 1, "right_operand": 2}
         response = self._client.calculate(incorrect_expression)
         assert (response.status_code == 400)
+        assert_valid_schema(response.json(), get_path_to_schema_file(__file__, 'calculate_error_400_response'))
+        # Я не уверен, что человеко-читаемые ошибки - это часть контракта.
+        assert (response.json()["error"] == "\"operation\" is not specified in request body")
+
